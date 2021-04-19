d5865a31df788d6bb2e3fc74e25a54e2d87328da
Joan Pedro <jpos.senai@gmail.com>
2021-04-19 08:40:03 -0300

    feat: Error handling on HttpClient service as observable using throwError operator with catchError

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { ProductListComponent } from './products/product-list.component';
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { StarComponent } from './shared/star/star.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { StarComponent } from './shared/star/star.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
