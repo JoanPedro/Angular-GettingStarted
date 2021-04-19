9fe65b1f34bb38c3da81fbf313177be488bd3c87
Joan Pedro <jpos.senai@gmail.com>
2021-04-19 20:57:41 -0300

    feat: adds Route guard for ProductDetail that check and alerts if invalid parameter id was passed

--- APM-Start/src/app/app-routing.module.ts
+++ APM-Start/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProductDetailGuard } from './products/shared/product.guard';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductDetailComponent } from './products/detail/product-detail.component';
 import { ProductListComponent } from './products/list/product-list.component';
@@ -6,19 +7,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 const productRouterChildren: Routes = [
   { path: '', component: ProductListComponent },
-  { path: ':id', component: ProductDetailComponent }
+  { path: ':id', component: ProductDetailComponent, canActivate: [ProductDetailGuard] }
 ];
 
 const routes: Routes = [
   { path: 'products', children: productRouterChildren },
   { path: 'welcome', component: WelcomeComponent },
-  { path: '', redirectTo: 'welcome', pathMatch: 'full'},
-  { path: '**', redirectTo: 'welcome',  pathMatch: 'full' },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
 
