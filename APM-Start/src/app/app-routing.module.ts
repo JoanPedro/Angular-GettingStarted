5ac4c91a974b39a60249db821e0c17fc1665c4a2
Joan Pedro <jpos.senai@gmail.com>
2021-04-19 19:51:01 -0300

    refactor: moves product children to its own variable

--- APM-Start/src/app/app-routing.module.ts
+++ APM-Start/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { ProductListComponent } from './products/list/product-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+const productRouterChildren: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: ':id', component: ProductDetailComponent }
+];
+
 const routes: Routes = [
-  { path: 'products', component: ProductListComponent },
-  { path: 'products/:id', component: ProductDetailComponent },
+  { path: 'products', children: productRouterChildren },
   { path: 'welcome', component: WelcomeComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'full'},
-  { path: '**', redirectTo: 'welcome' }
+  { path: '**', redirectTo: 'welcome' },
 ];
 
 @NgModule({
